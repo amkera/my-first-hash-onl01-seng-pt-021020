76d8d17967ac83474175fd6f54e7630580d1efbc
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-02-19 05:37:21 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,8 +1,11 @@
-def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+def my_hash()
+    your_hash = {
+        "name" => "amber", 
+        "age" => 25, 
+        "city" => "san francisco"    
+    }
 end
+
 
 
 def shipping_manifest
