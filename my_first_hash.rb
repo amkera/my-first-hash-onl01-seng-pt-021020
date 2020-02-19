a0cd12eb15dac90692113cc03c9a540e65909e22
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-02-19 05:43:48 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -31,18 +31,21 @@ end
 
 def adding
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
+      "whale bone corsets" => 5,
+      "porcelain vases" => 2,
+      "oil paintings" => 3
+  }
   # add 2 muskets to the shipping_manifest hash below
+  shipping_manifest["muskets"] = 2
 
 
 
   # add 4 gun powder to the shipping_manifest hash below
+  shipping_manifest["gun powder"] = 4
 
 
 
   # return the shipping_manifest hash below
+  return shipping_manifest
 
 end
